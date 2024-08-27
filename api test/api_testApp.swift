2795be72b64fff6423b3cb0940ae3e9d2e5d4666
Christian Wyglendowski <christian@dowski.com>
2024-08-27 09:30:45 -0400

    Initial Commit

---
+++ api test/api_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_testApp.swift
+//  api test
+//
+//  Created by Christian Wyglendowski on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct api_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
