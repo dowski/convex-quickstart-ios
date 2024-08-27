a83db6e89c4c404411668cff1ea1bf9e7ec230a5
Christian Wyglendowski <christian@dowski.com>
2024-08-27 15:27:50 -0400

    Shell of a demo app for using Convex w/ Swift on iOS

--- api test/ContentView.swift
+++ api test/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import ConvexMobile
+import Combine
+
+let client = ConvexClient(deploymentUrl: "https://prestigious-dolphin-371.convex.cloud")
 
 struct ContentView: View {
+    @State private var messages: [Task] = []
+    @State private var cancellationHandle: Set<AnyCancellable> = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        return List {
+            ForEach(messages) { message in
+                return Text(message.text)
+            }
+        }.task {
+            try! await client.subscribe(name: "tasks:get")
+                .replaceError(with: [Task(id: "id", isCompleted: false, text: "error")])
+                .receive(on: DispatchQueue.main)
+                .assign(to: \.messages, on: self)
+                .store(in: &cancellationHandle)
         }
         .padding()
+    }
+}
+
+struct Task : Identifiable, Decodable {
+    let id: String
+    let isCompleted: Bool
+    let text: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case isCompleted
+        case text
     }
 }
 
